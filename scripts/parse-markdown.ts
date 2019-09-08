2afb194093b4a01d8040fdd9fd0e5e595552b8c8
FatGe <18868877115@163.com>
2019-09-08 20:30:36 +0800

    feat(./websites): update html template

--- scripts/parse-markdown.ts
+++ scripts/parse-markdown.ts
@@ -16,45 +16,50 @@ import {
 const { promises: fs } = FileHandler;
 const md = new MarkdownIt();
 const towxml = new Towxml();
+const result: string[] = [];
 
-fs.readdir(questionsDir).then(async (files: string[]) => {
-    const readFilesAsync: Array<Promise<Buffer>> = files.map((file: string) =>
-        fs.readFile(resolve(file, questionsDir)),
-    );
-    const BufferFiles: Buffer[] = await Promise.all(readFilesAsync);
-    const result: string[] = [];
+const main = async () => {
+    const fileDirs: string[] = await fs.readdir(questionsDir);
+    const readDirAsync = fileDirs.map((fileDir: string) => {
+        return fs.readdir(resolve(fileDir, questionsDir)).then(async (files: string[]) => {
+            const readFilesAsync: Array<Promise<Buffer>> = files.map((file: string) =>
+                fs.readFile(resolve(`${fileDir}/${file}`, questionsDir)),
+            );
+            const BufferFiles: Buffer[] = await Promise.all(readFilesAsync);
 
-    BufferFiles.forEach(async (item: Buffer, i: number) => {
-        const str: string = item.toString();
-        const type: RegExpMatchArray = str.match(/(?<=(<!-- tags: \()).*(?=(\) -->))/g);
-        const file: string = files[i];
-        const html: string = md.render(str.replace(/(###)(.|\W)*/, ""));
-        const dom = new JSDOM(html);
-        const codes: HTMLElement[] = dom.window.document.querySelectorAll("pre code");
-        // highlight code block
-        codes.forEach((code: HTMLElement) => {
-            const { textContent, className } = code;
-            const lang: string = textToLang[className];
+            BufferFiles.forEach(async (item: Buffer, i: number) => {
+                const str: string = item.toString();
+                const type: RegExpMatchArray = str.match(/(?<=(<!-- tags: \()).*(?=(\) -->))/g);
+                const file: string = files[i];
+                const html: string = md.render(str.replace(/(###)(.|\W)*/, ""));
+                const dom = new JSDOM(html);
+                const codes: HTMLElement[] = dom.window.document.querySelectorAll("pre code");
+                // highlight code block
+                codes.forEach((code: HTMLElement) => {
+                    const { textContent, className } = code;
+                    const lang: string = textToLang[className];
 
-            code.innerHTML = Prism.highlight(textContent, Prism.languages[lang], lang);
+                    code.innerHTML = Prism.highlight(textContent, Prism.languages[lang], lang);
+                });
+                // serialize hmtl => string
+                const codeHtml = dom.serialize();
+                const content: object = { ...towxml.toJson(codeHtml), type, degree: fileDir };
+                // generate
+                result.push(JSON.stringify(content));
+                await fs.writeFile(resolve(mdToHtml(file), websitesDir), html);
+                await fs.writeFile(
+                    resolve(mdToAST(file), dataDir),
+                    JSON.stringify(content),
+                );
+            });
         });
-        // serialize hmtl => string
-        const codeHtml = dom.serialize();
-        const content: object = { ...towxml.toJson(codeHtml), type };
-        // generate
-        result.push(JSON.stringify(content));
-        await fs.writeFile(resolve(mdToHtml(file), websitesDir), html);
-        await fs.writeFile(
-            resolve(mdToAST(file), dataDir),
-            JSON.stringify(content),
-        );
     });
 
+    await Promise.all(readDirAsync);
     await fs.writeFile(
-        resolve("index.json", dataDir),
+        resolve(`index.json`, dataDir),
         result.join("\n"),
     );
-});
-// todo
-// * finish generate README
-// * finish questions
+};
+
+main();
